1caf52e2e7bfa5a1e7d9fe79e80798cb1f144549
chigaimasu <chigaimasu@localhost>
2012-01-11 14:13:26 -0500

    framework for CopyVPMJob

---
+++ vPowerModule/Cmdlets/CopyVPMJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Linq;
+using System.Text;
+using Veeam.Backup.Core;
+using vPowerModule;
+using System.Reflection;
+
+namespace vPowerModule.Cmdlets
+{
+    [Cmdlet("Copy","VPMJob")]
+    class CopyVPMJob : PSCmdlet
+    {
+        private vPowerModule.Objects.VPMJob _job;
+
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public vPowerModule.Objects.VPMJob Job { set { _job = value; } get { return _job; } }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            //COPY THAT JOB~!
+        }
+    }
+}
