a7306d72a5b9718d65e56296b6747fff8d8d0ca6
Justin Teager <Justin.Teager@vdell7zl6pm1.amust.local>
2012-01-11 14:04:10 -0500

    Initial Commit

---
+++ vPowerModule/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace vPowerModule
+{
+    public class Class1
+    {
+    }
+}
---
+++ vPowerModule/Cmdlets/GetVPMJob.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Linq;
+using System.Text;
+using Veeam.Backup.Core;
+using vPowerModule;
+using System.Reflection;
+
+namespace vPowerModule.Cmdlets
+{
+    [Cmdlet("Get","VPMJob")]
+    public class GetVPMJob: PSCmdlet
+    {
+        private string[] _name;
+        [Parameter(Position = 0, Mandatory = false)]
+
+        public string[] Name
+        {
+            get { return this._name; }
+            set { _name = value; }
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (this._name == null)
+                this.WriteObject((object)vPowerModule.Objects.VPMJob.GetAll(), true);
+            else
+            {
+
+                foreach (string pattern in this._name)
+                {
+                    if (pattern == null)
+                        this.WriteObject((object)vPowerModule.Objects.VPMJob.GetAll(), true);
+                    else
+                    {
+                        WildcardPattern wildcard = new WildcardPattern(pattern, WildcardOptions.Compiled | WildcardOptions.IgnoreCase);
+                        foreach (vPowerModule.Objects.VPMJob Job in (vPowerModule.Objects.VPMJob.GetAll()))
+                        {
+                            if (wildcard.IsMatch(Job.Name))
+                                WriteObject((object)Job, true);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
