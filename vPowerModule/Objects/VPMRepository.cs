0f1201b0977d44e1b7d7cbdf8539a5bc48141fb9
chigaimasu <chigaimasu@localhost>
2012-01-13 15:58:36 -0500

    More VPM repo stuff

--- vPowerModule/Objects/VPMRepository.cs
+++ vPowerModule/Objects/VPMRepository.cs
@@ -8,13 +8,50 @@ namespace vPowerModule.Objects
 {
     class VPMRepository
     {
-        private CBackupRepository _repository;
+        private readonly CBackupRepository _repository;
 
+        #region Properties
         public CBackupRepository Repository
         {
-            get { return _repository; }
-            set { _repository = value; }
+            get { return _repository; }           
         }
+
+        public string Description
+        {
+            get { return _repository.Description; }
+        }
+
+        public Guid HostId
+        {
+            get { return _repository.HostId; }
+        }
+
+        public Guid MountHostId
+        {
+            get { return _repository.MountHostId; }
+        }
+
+        public string Name
+        {
+            get { return _repository.Name; }
+        }
+
+        public CDomBackupRepositoryOptions Options
+        {
+            get { return _repository.Options; }
+        }
+
+        public string Path
+        {
+            get
+            { return _repository.Path; }
+        }
+
+        public Guid ShareCredsId
+        {
+            get { return _repository.ShareCredsId; }
+        }
+        #endregion
 
         public VPMRepository(CBackupRepository repo)
         {
