4ac7f5eae3f9e91c7554bc86d2d85aed01b5b698
chigaimasu <chigaimasu@localhost>
2012-01-13 15:20:45 -0500

    Added in Get-VPMRepository (untested)

---
+++ vPowerModule/Objects/VPMRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Veeam.Backup.Core;
+
+namespace vPowerModule.Objects
+{
+    class VPMRepository
+    {
+        private CBackupRepository _repository;
+
+        public CBackupRepository Repository
+        {
+            get { return _repository; }
+            set { _repository = value; }
+        }
+
+        public VPMRepository(CBackupRepository repo)
+        {
+            _repository = repo;
+        }
+    }
+}
