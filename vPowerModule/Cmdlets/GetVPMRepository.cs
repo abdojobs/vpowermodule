fd301929ed42e6ae8536ab30060a011bbf112a58
chigaimasu <chigaimasu@localhost>
2012-01-13 15:20:45 -0500

    Added in Get-VPMRepository (untested)

---
+++ vPowerModule/Cmdlets/GetVPMRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using Veeam.Backup.Core;
+
+namespace vPowerModule.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get,"VPMRepository")]
+    class GetVPMRepository : PSCmdlet
+    {
+
+        protected override void ProcessRecord()
+        {
+            foreach (CBackupRepository repo in CBackupRepository.GetAll())
+            {
+                this.WriteObject((object)new vPowerModule.Objects.VPMRepository(repo));
+            }
+        }
+    }
+}
