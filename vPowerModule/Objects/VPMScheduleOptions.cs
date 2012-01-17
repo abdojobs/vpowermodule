fab022b6b50907a14233a4292d87e374b044d379
sethbartlett <sethbartlett@localhost>
2012-01-17 12:35:52 -0500

    Merge

---
+++ vPowerModule/Objects/VPMScheduleOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Veeam.Backup.Model;
+
+namespace vPowerModule.Objects
+{
+    public class VPMScheduleOptions
+    {
+        private ScheduleOptions _schedOptions;
+
+        public ScheduleOptions SchedOptions
+        {
+            get { return _schedOptions; }
+            set { _schedOptions = value; }
+        }
+        
+
+        public VPMScheduleOptions(ScheduleOptions scheduleOptions)
+        {
+            // TODO: Complete member initialization
+            this._schedOptions = scheduleOptions;
+        }
+    }
+}
