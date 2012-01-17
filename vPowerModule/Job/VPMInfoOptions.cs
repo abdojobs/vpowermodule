b25990e420fbb9795554b94ce9a549e87b6b4801
sethbartlett <sethbartlett@localhost>
2012-01-17 13:11:19 -0500

    Merge

---
+++ vPowerModule/Job/VPMInfoOptions.cs
@@ -0,0 +1,21 @@
+﻿using Veeam.Backup.Common;
+
+namespace vPowerModule.Job
+{
+    public class VPMInfoOptions
+    {
+        private CDomContainer _options;
+
+        public CDomContainer Options
+        {
+            get { return _options; }
+            set { _options = value; }
+        }
+
+        public VPMInfoOptions(CDomContainer cDomContainer)
+        {
+            // TODO: Complete member initialization
+            this._options = cDomContainer;
+        }
+    }
+}
---
+++ vPowerModule/Job/VPMScheduleOptions.cs
@@ -0,0 +1,22 @@
+﻿using Veeam.Backup.Model;
+
+namespace vPowerModule.Job
+{
+    public class VPMScheduleOptions
+    {
+        private ScheduleOptions _schedOptions;
+
+        public ScheduleOptions SchedOptions
+        {
+            get { return _schedOptions; }
+            set { _schedOptions = value; }
+        }
+        
+
+        public VPMScheduleOptions(ScheduleOptions scheduleOptions)
+        {
+            // TODO: Complete member initialization
+            this._schedOptions = scheduleOptions;
+        }
+    }
+}
